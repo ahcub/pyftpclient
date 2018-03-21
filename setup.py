afe46d5e475e2d92cb034e9c1bab0c0e79debcd0
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-03-22 01:27:16 +0400

    !U update the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='pyftpclient',
     packages=find_packages(include=('pyftpclient',)),
-    version='0.0.1',
+    version='0.0.2',
     description='ftp client wrapper to simplify working with paramiko or ftplib',
     author='Alex Buchkovsky',
     author_email='olex.buchkovsky@gmail.com',
