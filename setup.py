8421c784db428788a1945e9f6b0a639fdecba311
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-03-29 15:05:50 +0300

    !U add requirements

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ from setuptools import find_packages
 setup(
     name='pyftpclient',
     packages=find_packages(include=('pyftpclient',)),
-    version='0.0.3',
+    version='0.0.4',
     description='ftp client wrapper to simplify working with paramiko or ftplib',
     author='Alex Buchkovsky',
     author_email='olex.buchkovsky@gmail.com',
     url='https://github.com/ahcub/pyftpclient',
     keywords=['wrapper', 'sftp', 'client', 'ftp'],
+    install_requires=[
+        'paramiko',
+        'os_utils',
+    ],
 )
