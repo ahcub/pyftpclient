732937bb4380291735337a5df47eb99431a90185
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-03-29 15:28:35 +0300

    !U update package requirements and publishing workflow

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from distutils.core import setup
+from os.path import join, dirname
 
 from setuptools import find_packages
+
+with open(join(dirname(__file__), 'requirements.txt')) as requirements_file:
+    install_reqs = [line.strip() for line in requirements_file]
+
+print('install_reqs:', install_reqs)
 
 setup(
     name='pyftpclient',
     packages=find_packages(include=('pyftpclient',)),
-    version='0.0.4',
+    package_data={'': ['requirements.txt', 'LICENCE']},
+    include_package_data=True,
+    version='0.1.1',
     description='ftp client wrapper to simplify working with paramiko or ftplib',
     author='Alex Buchkovsky',
     author_email='olex.buchkovsky@gmail.com',
     url='https://github.com/ahcub/pyftpclient',
     keywords=['wrapper', 'sftp', 'client', 'ftp'],
-    install_requires=[
-        'paramiko',
-        'os_utils',
-    ],
+    install_requires=install_reqs,
 )
