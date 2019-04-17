030b0a0b3594089dbce9240b616b2a7bf5387cd4
Alex Buchkovsky <alex.buchkovsky@gmail.com>
2019-04-17 22:53:48 +0300

    increase the version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=find_packages(include=('pyftpclient',)),
     package_data={'': ['requirements.txt', 'LICENCE']},
     include_package_data=True,
-    version='0.1.10',
+    version='0.1.11',
     description='ftp client wrapper to simplify working with paramiko or ftplib',
     long_description=long_description,
     long_description_content_type='text/markdown',
